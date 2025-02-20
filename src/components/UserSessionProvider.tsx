a3f6032a5fc8e3064deb6ee6a56ac5f690c8d1f4
Salman <salmankps2001@gmail.com>
2025-02-21 02:40:21 +0530

    unchanged setup

--- src/components/UserSessionProvider.tsx
+++ src/components/UserSessionProvider.tsx
@@ -1,7 +1,8 @@
 'use client';
 
+import { getBaseUrl } from '@/utils/getBaseUrl';
 import { SessionProvider } from 'next-auth/react';
 
 export default function UserSessionProvider({ children }: { children: React.ReactNode }) {
-  return <SessionProvider basePath="/api/auth">{children}</SessionProvider>;
+  return <SessionProvider basePath={getBaseUrl()}>{children}</SessionProvider>;
 }
